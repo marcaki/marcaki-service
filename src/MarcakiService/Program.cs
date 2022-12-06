703fa1b09052d88667a55ab4a1b3aad7ea6c335e
sinedolvra <francisco.lima@stone.com.br>
2022-12-06 10:42:26 -0300

    Remove code smell

--- src/MarcakiService/Program.cs
+++ src/MarcakiService/Program.cs
@@ -26,7 +26,6 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    // app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 }
 
 app.UseHttpsRedirection();
