474dd41da9f1579d4c66338c48023b6f299baf32
sinedolvra <francisco.lima@stone.com.br>
2022-12-05 21:00:51 -0300

    Add healthcheck endpoint

---
+++ src/MarcakiService.Cross/Extensions/HealthCheckExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MarcakiService.Cross.Extensions;
+
+public static class HealthCheckExtensions
+{
+    public static void ConfigureHealthChecks(this IHealthChecksBuilder builder, IConfiguration configuration)
+    {
+        builder.AddCheck("self", () => HealthCheckResult.Healthy());
+        builder.AddSqlServer(
+            connectionString: configuration["ConnectionStrings:MarcakiDb"],
+            name: "MarcakiDb"
+        );
+        builder.Services.AddHealthChecksUI(options =>
+        {
+            options.SetEvaluationTimeInSeconds(int.Parse(configuration["HealthCheckConfig:EvaluationTimeInSeconds"]));
+            options.MaximumHistoryEntriesPerEndpoint(int.Parse(configuration["HealthCheckConfig:MaximumHistoryEntriesPerEndpoint"]));
+            options.AddHealthCheckEndpoint(configuration["HealthCheckConfig:Description"], configuration["HealthCheckConfig:Url"]);
+        })
+        .AddInMemoryStorage();
+    }
+}
--- src/MarcakiService/Program.cs
+++ src/MarcakiService/Program.cs
@@ -1,4 +1,15 @@
+using HealthChecks.UI.Client;
+using MarcakiService.Cross.Extensions;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false, true)
+    .AddEnvironmentVariables()
+    .Build();
 
 // Add services to the container.
 
@@ -6,6 +17,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks().ConfigureHealthChecks(configuration);
 
 var app = builder.Build();
 
@@ -17,9 +29,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseHealthChecks("/health", new  HealthCheckOptions
+{
+    Predicate = p => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.UseHealthChecksUI(options => { options.UIPath = "/dashboard"; });
 
 app.Run();
