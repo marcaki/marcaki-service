cf9a5e59c12a984574c649d3abb2a83c70c19f7b
sinedolvra <francisco.lima@stone.com.br>
2022-12-07 22:38:10 -0300

    Add initial validation

---
+++ src/MarcakiService/Contracts/Validators/CreateProviderRequestValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace MarcakiService.Application.Contracts.Validators;
+
+public class CreateProviderRequestValidator : AbstractValidator<CreateProviderRequest>
+{
+    public CreateProviderRequestValidator()
+    {
+        RuleForEach(x => x.Availability).NotEmpty().SetValidator(new AvailabilityRequestValidator());
+    }
+}
