ffc6e1ab667df9dfff92702bc277925abde419c9
sinedolvra <francisco.lima@stone.com.br>
2022-12-07 22:37:59 -0300

    Create ProviderController.cs

---
+++ src/MarcakiService/Controllers/ProviderController.cs
@@ -0,0 +1,14 @@
+﻿using MarcakiService.Application.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarcakiService.Application.Controllers;
+
+[Route("api/v1/provider")]
+public class ProviderController : Controller
+{
+    [HttpPost]
+    public IActionResult CreateProvider([FromBody] CreateProviderRequest request)
+    {
+        return Accepted();
+    }
+}
