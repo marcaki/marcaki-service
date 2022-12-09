ebdac22073b9bd2f921248fe5d878a133a4bb058
sinedolvra <francisco.lima@stone.com.br>
2022-12-08 23:19:25 -0300

    Add unit tests

---
+++ tests/MarcakiService.Application.UnitTests/Validators/CreateProviderRequestValidatorTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation.TestHelper;
+using MarcakiService.Application.Contracts;
+using MarcakiService.Application.Contracts.Validators;
+using Xunit;
+
+namespace MarcakiService.Application.UnitTests.Validators;
+
+public class CreateProviderRequestValidatorTests
+{
+    private CreateProviderRequestValidator Validator { get; set; } = new();
+
+    [Fact]
+    public void Validate_GivenAnInvalidId_ShouldHaveValidationError()
+    {
+        var request = new CreateProviderRequest(){Id = ""};
+
+        var result = Validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Id);
+    }
+}
