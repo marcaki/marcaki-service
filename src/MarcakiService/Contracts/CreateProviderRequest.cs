c5300d09fc25621981e916243cc0f8e31c050948
sinedolvra <francisco.lima@stone.com.br>
2022-12-08 23:19:16 -0300

    Add validation

--- src/MarcakiService/Contracts/CreateProviderRequest.cs
+++ src/MarcakiService/Contracts/CreateProviderRequest.cs
@@ -1,4 +1,7 @@
-﻿namespace MarcakiService.Application.Contracts;
+﻿using FluentValidation.Results;
+using MarcakiService.Application.Contracts.Validators;
+
+namespace MarcakiService.Application.Contracts;
 
 public class CreateProviderRequest
 {
@@ -8,12 +11,17 @@ public class CreateProviderRequest
     public List<PhoneRequest> Phones { get; set; }
     public string Email { get; set; }
     public List<string> Address { get; set; }
-    public List<int> Employees { get; set; }
-    public List<int> Services { get; set; }
+    public List<string> Employees { get; set; }
+    public List<string> Services { get; set; }
     public List<AvailabilityRequest> Availability { get; set; }
     public string Status { get; set; }
     public string ActivationDate { get; set; }
     public string BlockedDate { get; set; }
     public string CreationDate { get; set; }
     public string BlockedReason { get; set; }
+
+    public ValidationResult Validate()
+    {
+        return new CreateProviderRequestValidator().Validate(this);
+    }
 }
--- src/MarcakiService/Controllers/ProviderController.cs
+++ src/MarcakiService/Controllers/ProviderController.cs
@@ -1,4 +1,5 @@
 ﻿using MarcakiService.Application.Contracts;
+using MarcakiService.Application.Contracts.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MarcakiService.Application.Controllers;
@@ -9,6 +10,11 @@ public class ProviderController : Controller
     [HttpPost]
     public IActionResult CreateProvider([FromBody] CreateProviderRequest request)
     {
+        var validationResult = request.Validate();
+        if (validationResult.IsValid == false)
+        {
+            return new BadRequestObjectResult(new InvalidRequestResponse(validationResult));
+        }
         return Accepted();
     }
 }
